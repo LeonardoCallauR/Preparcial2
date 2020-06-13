f8c591257185306b3d5670e8602c1865a29a4cdd
LeonardoCallauR <61084776+LeonardoCallauR@users.noreply.github.com>
2020-06-13 05:53:30 -0400

    Primer paso

---
+++ AppCountries/AppCountries/AppCountries/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppCountries.Models;
+
+namespace AppCountries.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
